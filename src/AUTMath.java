f65f1934e81e0507e0bc3b13ac2e108e36280c9d
elenahedayat <elenahedayat5gmail.com>
2024-04-30 10:40:27 -0700

    fix code

--- src/AUTMath.java
+++ src/AUTMath.java
@@ -21,7 +21,7 @@ public class AUTMath {
      */
     public static int subtract(int num1, int num2) {
         int result = num1 - num2;
-        return Math.abs(result);
+        return (result);
     }
 
     /**
@@ -32,10 +32,19 @@ public class AUTMath {
      */
     public static int multiply(int num1, int num2) {
         int result = 0;
-        for (int i = 0; i <= num2; i++) {
-           result += num1;
+        if(num2>=0) {
+            for (int i = 0; i < num2; i++) {
+                result += num1;
+            }
+            return result;
         }
-        return result;
+        else{
+            for (int i = 0; i > num2; i--) {
+                result += num1;
+            }
+            return (-1)*result;
+        }
+
     }
 
     /**
@@ -45,6 +54,8 @@ public class AUTMath {
      * @return The result of dividing the first parameters by the second parameter
      */
     public static int divide(int num1, int num2) {
+        if(num2==0)
+            return num1;
         return num1 / num2;
     }
 
@@ -55,7 +66,7 @@ public class AUTMath {
      */
     public static int factorial(int number) {
         if (number == 0) {
-            return number;
+            return 1;
         } else {
             return number * factorial(number - 1);
         }
@@ -63,11 +74,12 @@ public class AUTMath {
 
     /**
      * power operand
-     * @param base the base operator
+     *
+     * @param base  the base operator
      * @param power the power operator
      * @return the result of raising the first parameter to the power of the second parameter.
      */
     public static int pow(int base, int power) {
-        return base * pow(base, power - 1);
+        return (int) (base *( Math.pow(base, (power - 1))));
     }
 }
